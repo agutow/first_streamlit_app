31f351e309db237b232f73fffac710ad3232f1b5
Alex Gutow <alex.gutow@gmail.com>
2023-07-25 18:39:27 -0700

    Update streamlit_app.py
    makes api readable

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,5 +27,10 @@ streamlit.header('Fruityvice Fruit Advice!')
 
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response.json())
+streamlit.text(fruityvice_response.json()) #just writes data to screen
+
+# normalizes json data
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+# outputs in app as table
+streamlit.dataframe(fruityvice_normalized)
 
